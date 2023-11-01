58ec24c18cbc53117b1fc55e09bbc0a1ac4b99a9
YaeliFishman <y0533197798@gmail.com>
2023-11-01 18:24:27 +0200

    my first commit

---
+++ server-calendar/Controllers/EventsController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace server_calendar.Controllers
+{ 
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+        private static List<Event> events = new List<Event> 
+        { 
+            //new Event { Id = 1, Title = "wedding" ,Start = new DateTime(2023,09,11)} ,
+            //new Event { Id = 2, Title = "chanuca", Start = new DateTime(2023,09,25) },
+            //new Event { Id = 3, Title = "birthday" ,Start = new DateTime(2023,09,10)},
+            //new Event { Id = 4, Title = "course" ,Start = new DateTime(2023,09,14)} 
+        };
+
+        // GET: api/<EventsController>
+        [HttpGet]
+
+        public IEnumerable<Event> Get()
+        {
+            return events;
+        }
+
+        // GET api/<EventsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+        
+        // POST api/<EventsController>
+        [HttpPost]
+        public void Post([FromBody] Event myEvent)
+        {
+            events.Add(new Event { Id=4,Title=myEvent.Title,Start=myEvent.Start});
+            return;
+        }
+       
+        // PUT api/<EventsController>/5
+        [HttpPut("{id}")]
+        public Event Put(int id, [FromBody] Event updateEvent)
+        {
+            var e = events.Find(e => e.Id == id);
+            e.Title = updateEvent.Title;
+            return e;
+        }
+        
+        // DELETE api/<EventsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var e = events.Find(e => e.Id == id);
+            events.Remove(e);
+        }
+    }
+}
